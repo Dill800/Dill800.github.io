52800ffddbbae3de712ad5fb72b5ee9d10d20fbb
Dillon McGovern <dillonm800@gmail.com>
2022-08-27 19:23:38 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.afdcd582b3eb0194347db569b960f651.js"
+  "/precache-manifest.5f507976044ec22407e07307562ef29f.js"
 );
 
 self.addEventListener('message', (event) => {
